5650a68340a28daaf9c039fce3ba7f84068defe5
kHRYSTAL <723526676@qq.com>
2016-01-04 10:15:29 +0800

    push demo

---
+++ app/src/main/java/com/yao/qrcodescan/MainActivity.java
@@ -0,0 +1,92 @@
+package com.yao.qrcodescan;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.shandianshua.jarvis.QRScanActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final static int SCANNIN_GREQUEST_CODE = 1;
+    /**
+     * 显示扫描结果
+     */
+    private TextView mTextView ;
+    /**
+     * 显示扫描拍的图片
+     */
+    private ImageView mImageView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextView = (TextView) findViewById(R.id.result);
+        mImageView = (ImageView) findViewById(R.id.qrcode_bitmap);
+
+        //点击按钮跳转到二维码扫描界面，这里用的是startActivityForResult跳转
+        //扫描完了之后调到该界面
+        Button mButton = (Button) findViewById(R.id.button1);
+        mButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this, QRScanActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivityForResult(intent, SCANNIN_GREQUEST_CODE);
+            }
+        });
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case SCANNIN_GREQUEST_CODE:
+                if(resultCode == QRScanActivity.RESULT_CODE){
+                    Bundle bundle = data.getExtras();
+                    //显示扫描到的内容
+                    mTextView.setText(bundle.getString("scan_result"));
+                    //显示
+                   // mImageView.setImageBitmap((Bitmap) data.getParcelableExtra("bitmap"));
+                }
+                break;
+        }
+    }
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
